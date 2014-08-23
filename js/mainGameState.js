dc77d970a2a5eb4c144420bb4c19f48e57f7eeb9
Tom Davies <tod502@york.ac.uk>
2014-08-23 15:13:07 +0100

    Add jump jet animation

--- js/mainGameState.js
+++ js/mainGameState.js
@@ -30,6 +30,7 @@ MainGameState.prototype.create = function() {
     this.player.anchor.setTo(0.5,0.5);
     this.player.animations.add('run', [0, 1, 2, 3], 10, true);
     this.player.animations.add('stop', [0], 10, true);
+    this.player.animations.add('jet', [4, 5], 10, true);
     this.game.camera.follow(this.player);
     this.cursors = this.game.input.keyboard.createCursorKeys();
     this.music = this.game.add.audio('overworld');
@@ -39,16 +40,32 @@ MainGameState.prototype.create = function() {
 MainGameState.prototype.update = function() {
     this.game.physics.arcade.collide(this.player, this.layer);
     this.player.body.velocity.x = 0;
+    
+    if (this.cursors.up.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpEnd)
+    {
+        this.player.body.velocity.y = -280;
+        this.jumpEnd = this.game.time.now + 1000;
+        this.jumping = true;
+        this.player.animations.play('jet');
+    }
+    else if (!this.cursors.up.isDown && this.player.body.onFloor())
+    {
+        this.jumping = false;
+    }
+    
     if (this.cursors.left.isDown)
     {
         this.player.body.velocity.x = -150;
-        if (this.player.body.onFloor())
+        if (!this.jumping)
         {
-            this.player.animations.play('run');
-        }
-        else
-        {
-            this.player.animations.stop();
+            if (this.player.body.onFloor())
+            {
+                this.player.animations.play('run');
+            }
+            else
+            {
+                this.player.animations.stop();
+            }
         }
         if (this.facing != 'left')
         {
@@ -60,6 +77,8 @@ MainGameState.prototype.update = function() {
     {
         this.player.body.velocity.x = 150;
 
+        if (!this.jumping)
+        {
             if (this.player.body.onFloor())
             {
                 this.player.animations.play('run');
@@ -68,24 +87,18 @@ MainGameState.prototype.update = function() {
             {
                 this.player.animations.stop();
             }
-        
+        }
         if (this.facing != 'right')
         {
             this.player.scale.x = -Math.abs(this.player.scale.x);
             this.facing = 'right';
         }
     }
-    else
+    else if (!this.jumping)
     {
         this.facing = 'none';
         this.player.animations.stop();
         this.player.frame = 0;
-    }
-
-    if (this.cursors.up.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpEnd)
-    {
-        this.player.body.velocity.y = -280;
-        this.jumpEnd = this.game.time.now + 1000;
     }
 };
 
