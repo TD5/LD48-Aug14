3802cfa80922f96ae6ed28d088a09d409e04e842
Tom Davies <tod502@york.ac.uk>
2014-08-23 11:29:20 +0100

    Add main game state

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 var game = new Phaser.Game(850, 550, Phaser.AUTO, 'game', null, false, false);
 var mainMenuState = new MainMenuState(game);
+var mainGameState = new MainGameState(game);
 game.state.add('mainMenu', mainMenuState);
+game.state.add('mainGame', mainGameState);
 game.state.start('mainMenu');
---
+++ js/mainGameState.js
@@ -0,0 +1,21 @@
+function MainGameState(game)
+{
+    this.game = game;
+    this.music = undefined;
+}
+
+MainGameState.prototype.preload = function() {
+
+};
+
+MainGameState.prototype.create = function() {
+
+};
+
+MainGameState.prototype.update = function() {
+
+};
+
+MainGameState.prototype.render = function() {
+
+};
