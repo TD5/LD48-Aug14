900e576d4d646f5f1f14705f6c554be31933db6f
Tom Davies <tod502@york.ac.uk>
2014-08-24 13:51:14 +0100

    Add vertical accelerator platform tiles

--- js/mainGameState.js
+++ js/mainGameState.js
@@ -27,7 +27,7 @@ MainGameState.prototype.create = function()
     this.map = this.game.add.tilemap('lvl1');
     this.map.addTilesetImage('lvl1tiles');
     this.map.setCollision([1, 3, 5, 6, 7, 8, 9, 10, 11, 15, 16, 17, 18], true);
-    this.map.setCollision([2, 4, 12, 13, 14, 19, 20, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40], false);
+    this.map.setCollision([2, 4, 12, 13, 14, 19, 20, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40], false);
     this.layer = this.map.createLayer('layer1');
     this.layer.resizeWorld();
     this.game.physics.arcade.gravity.y = 250;
